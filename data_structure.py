e7ede49d46c36149f674831de917696a0d0d2002
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-19 20:41:45 +0530

    insert,append,remove,pop functions add and check

--- data_structure.py
+++ data_structure.py
@@ -1,3 +1,13 @@
 x = [12, 45, 85, 75, 78]
-y = x[2]
-print(y)
+x.append(100)
+#x.append(4567)
+#x.insert(4712)
+x.insert(2, 897)
+#y = x[3]
+#print(y)
+
+x.remove(78)
+x.pop(1)
+
+#x[4] = 789
+print(x)
