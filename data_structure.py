29dc33f3ffd699a14b8ce5e40d523945d8e6ffe9
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-19 20:45:35 +0530

    made z list and check in,not in functions

--- data_structure.py
+++ data_structure.py
@@ -1,13 +1,23 @@
 x = [12, 45, 85, 75, 78]
+y = [0, 10, 20, 30, 40]
+
+z = x + y
+print(z)
+
 x.append(100)
+
 #x.append(4567)
 #x.insert(4712)
+
 x.insert(2, 897)
 #y = x[3]
 #print(y)
 
 x.remove(78)
+print(x)
 x.pop(1)
 
 #x[4] = 789
 print(x)
+print(10 in z)
+print(789 not in z)
