85f08551ae74cb94d89151b996f88cc1a4ddeb04
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-24 16:18:49 +0530

    check numbers

--- set2.py
+++ set2.py
@@ -2,5 +2,10 @@ x = {"Kill", "Null", "Will", "Hill"}
 y = {"1", "2", "3"}
 t = {"Null","Will"}
 
+u = {"1": 1, "2": 2, "3": 3}
+
 z = x - y - t
 print(z)
+print("1" in y)
+print("1" not in y)
+print("1" in u)
