af4169bc5ea5c0c2ea8583906e0ac182fcdef4b8
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-24 10:46:37 +0530

    dictionary example update

--- dictonary.py
+++ dictonary.py
@@ -1,6 +1,12 @@
 x = {'1':"Colombo", '2':"Kalutara"}
 x['3']= "moratuwa"
+x['Cities'] = ["panadura","yapanaya"]
+y = x['Cities']
+x['names'] = {'0':"Menuka", '1':"Banuka"}
+z = x['names']
 
+print(z)
+print(y)
 print(x)
 print(x.keys())
 print(x.values())
