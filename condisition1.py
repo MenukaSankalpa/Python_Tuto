ce08bdf6ee01e8bdd8f8e7cd652f1f0e1a2a2c96
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-25 09:11:28 +0530

    make conditions

---
+++ condisition1.py
@@ -0,0 +1,15 @@
+mark = 75
+
+if mark >=75:
+    print("Grade A")
+else: 
+    print("Grade B") 
+    
+
+M = 75
+result = "" 
+if M>=75:
+    result = "Pass"
+else:
+    result = "Fail"
+print (result)              
---
+++ condition.py
@@ -0,0 +1,7 @@
+
+x = int(input("Enter your Number: "))
+
+if x>=5:
+    print("x is yes")
+else: 
+    print("x is no")
