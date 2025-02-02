7cf0da599a94046ff5522ce52d3a7c7aa7955e36
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-02-02 09:33:40 +0530

    example create

---
+++ break_continue.py
@@ -0,0 +1,18 @@
+x = [-12, -23, -567, -123, -8]
+
+count = 0
+
+'''while True:
+    if count == 10:
+        break
+    
+    print("Count ", count)
+    count += 1 '''
+    
+    
+while True:
+    if count >= 10:
+        break
+    
+    print("Count ", count)
+    count += 3  
